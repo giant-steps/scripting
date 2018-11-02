a175c3b7d09c91b7859bc64da231a9a95444ff19
giant-steps <chris@chris-laptop>
2018-11-02 12:01:06 -0700

    added second file, updated instructions

---
+++ hw5.2.py
@@ -0,0 +1,36 @@
+"""
+This program will answer part two of hw5 for scripting
+
+4) Create and draw a random graph with 10 nodes (show the node labels on the drawing and save
+the figure to a file).
+
+5) Run a depth-first search on the random graph. Show the order in which the nodes were visited.
+
+6) Run a breadth-first search on the random graph. Show the order in which the nodes were
+visited.
+
+7) Use either search algorithm to determine whether the random graph is connected (DON'T use the NetworkX is_connected()
+function).
+
+8) Also, do 5 through 7 above using the example karate club graph shown in class (start at node 0).
+
+turn in:
+1. A Python module containing the functions specified above (from part 1).
+2. A word document showing the commands for importing and using the module, along with the
+output for 4-8 above (please paste the figure of the random graph in this document).
+
+"""
+
+## import statements
+import sys
+
+## function / class definitions
+
+## main function definition
+def main():
+    pass
+
+## run main function
+if __name__ == "__main__":
+    main()
+
